309a4a1539dd2586af9ce8753a34c02fbdfe1d87
João Marcelo Dantas <jmgrd98@gmail.com>
2022-06-17 16:25:33 -0300

    background color commit

--- script.js
+++ script.js
@@ -9,17 +9,20 @@ mensagem.innerHTML= `Agora são ${hora} horas.`
 
 if(hora >= 6 && hora < 12){
     foto.src = "manha.jpg";
-    frase.innerText = "O dia está começando, prepare um café e aproveite!"
+    frase.innerText = "O dia está começando, prepare um café e aproveite!";
+    document.body.style.background = '#a5e7ff';
 }
 
-else if (hora >= 12 && hora < 18){
+else if (hora >= 12 && hora <= 18){
     foto.src = "tarde.jpg";
-    frase.innerText = "Agora é tarde, uma boa hora para ler um livro e relaxar."
+    frase.innerText = "Agora é tarde, uma boa hora para ler um livro e relaxar.";
+    document.body.style.background = '#e89200';
 }
 
 else {
     foto.src = "noite.jpg";
     frase.innerText = "A noite chegou e com ela as estrelas. Admire o céu por um momento."
+    document.body.style.background = '#6600ba';
 }
 
 
