40272850321b78ce51428329cd18f49a0edfab45
João Marcelo Dantas <jmgrd98@gmail.com>
2022-06-16 22:40:52 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function carregar(){
+
+let mensagem = document.getElementById('mensagem');
+let foto = document.getElementById('foto')
+let data = new Date();
+let hora = data.getHours();
+mensagem.innerHTML= `Agora são ${hora} horas.`
+
+if(hora >= 0 && hora < 12){
+    foto.src = "manha.jpg";
+}
+
+else if (hora >= 12 && hora < 18){
+    foto.src = "tarde.jpg";
+}
+
+else {
+    foto.src = "noite.jpg";
+}
+
+
+}
+
